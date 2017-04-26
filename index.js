bfeba8ec59d7fab149e12aebbbf6cd7929ebdb09
Alexander Brueckner <Alexander.Brueckner@student.reutlingen-university.de>
2017-04-26 21:51:01 +0200

    No idea why blowmix did not commit this to git as well

--- index.js
+++ index.js
@@ -8,11 +8,35 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var express = require('express');
+var mysql = require('mysql');
 //3 Arrays, usage explained further down 
 var sockets = [];
 var names = [];
 var clients = [];
+var url = "https://27e4e9e0-5c29-4fba-bc97-43f438cfd4a0-bluemix.cloudant.com/chatusers/test";
+var port = process.env.VCAP_APP_PORT || 8080;
+var crypto = require('crypto');
+	
+//Database Setup
+var dbconnection;
+var pool;
 
+//bind the exit/uncaughtexception/sigint events 
+process.on('exit',function(){
+	console.log("PROCESS EXITING!");
+});
+
+process.on('uncaughtException',function(e){
+	console.log("Killed Database Connection!");
+	console.log("UNCAUGHT EXCEPTION: ");
+	console.log(e.stack);
+	process.exit(99);
+});
+
+process.on('SIGINT',function(){
+	console.log("SIGINT RECEIVED!");
+	process.exit(99);
+});
 //Utility function.. Somewhat useless as we can inline this, then again, what are interpreters for?
 function getUsers(){
 return clients.toString();
@@ -69,29 +93,131 @@ app.get('/chat.html', function(req, res){
 //Main socket event handler
 io.on('connection', function(socket){
 //When somebody happens to join us, let everyone else know and register it on the server
-	socket.on('clientjoin',function(clientName){
+
+	//Ask mister database if this guy exists, if not, create him in there..
+	socket.on('validateuserdata',function(data){
+		
+				pool.getConnection(function(err,dbconnection){
+					if(err){
+						throw err;
+					}
+					
+				var sqlquery_retrieve = 'select * from user where nickname = ' + dbconnection.escape(data.username);
+				dbconnection.query(sqlquery_retrieve,
+					function(error,results,fields){
+					dbconnection.release();
+					if(error){
+						console.log("Error during insertion:");
+						console.log(error);
+						throw error;
+					}
+				
+				//console.log("Query Results:");
+				if(results[0]){
+				//console.log("Result 0 nickname: " +results[0].nickname);
+				//console.log("Result 0 password: " +results[0].password);
+				//console.log("Entered username: " + data.username);
+				//console.log("Entered Password: " + data.userpassword);
+				//console.log("Hashed Password: " + crypto.createHash('sha256').update(data.userpassword).digest('hex'));
+				
+				//user exists
+				
+					//Storing passwords in plaintext is retarded. MD5 is effectively useless by now, so SHA256.
+					//We compare the two hashes. TODO: Salt them
+					if(crypto.createHash('sha256').update(data.userpassword).digest('hex') == results[0].password){
+						socket.emit('validationresult',{success : "true"});
+					}
+					else{
+						socket.emit('validationresult',{success : "false"});
+					}
+					
+				}
+				//user does not exist
+				else{
+					
+					console.log("user does not exist");
+					socket.emit('validationresult',{success : "false"});
+					
+				}
+				
+							
+			});
+					
+					
+				});
+	
+	});
+	//Register event. Connects to the database, checks if the name is taken. Will then proceed to either
+	//yell at the user or register the new user in the database.
+	socket.on('register',function(data){	
+	pool.getConnection(function(err,dbconnection){	
+	
+					var allowRegistration = true;
+					var tempName = data.username;
+					var tempPw = data.userpassword;
+					var sqlquery_checkexists = 'select * from user where nickname = ' + dbconnection.escape(tempName);
+					dbconnection.query(sqlquery_checkexists,function(err,results,fields){
+						if(results[0]){
+							if(results[0].nickname === tempName){
+								socket.emit('registrationresult',{success : "false"});
+								allowRegistration = false;
+								return;
+							}
+						}
+					//Cleared to register
+					if(allowRegistration){
+					var sqlquery_insert = 'insert into user values('+dbconnection.escape(tempName)+','+'SHA2('+dbconnection.escape(tempPw)+',256))';
+					console.log(sqlquery_insert);
+					//Execute query, close connection
+					dbconnection.query(sqlquery_insert,function(error,results,fields){
+						dbconnection.release();
+						if(error){
+							console.log("Error during insertion:");
+							console.log(error);
+							socket.emit('registrationresult',{success : "false"});
+							return;
+							//throw error;
+						}
+						console.log("new user registered!");		
+					});
+					
+					socket.emit('registrationresult',{success : "true"});
+					}
+					//Should registration fail, release connection
+					else{
+						dbconnection.release();
+					}
+						
+						
+					});
+					
+
+	});
+	});
+
+	socket.on('clientjoin',function(clientData){	
 	//Assuming his name is not yet taken..
-	if(clients.indexOf(clientName) > -1){
+	if(clients.indexOf(clientData.nickname) > -1){
 		socket.emit('nametaken','Username already taken');
 		return;
-	}
+	}	
 	//I had a dilemma. Either use a O(n) worst-case loop to find the socket when he
 	//disconnects, or save it in both ways.. 'We have so much memory that nobody
 	//cares about storage efficiency anymore' - Someone on the internet
 
 	//socket[name] -> Mapping of Names to Sockets
-	sockets[clientName] = socket;
+	sockets[clientData.nickname] = socket;
 	//names[socket] -> Mapping of socket-ids to their names (Arrays here are KIND of a map,
 	//but also too "arrayly" to behave like an actual map.. )
-	names[socket.id] = clientName;
+	names[socket.id] = clientData.nickname;
 	//And just for the sake of easy access, store the name a 2nd time in a "normal" array
 	//this seems like a waste of memory, but it also removes the burden of dealing with JSON 
 	//when trying to list all the users. So it is a tradeoff between memory wasting and less code.
-	clients.push(clientName);
+	clients.push(clientData.nickname);
 	//Since we are now part of the chat, let's let everybody know we're here
-	io.emit('serverannouncement',displayTime() + ' User connected: ' + clientName + ' - Welcome!');
+	io.emit('serverannouncement',displayTime() + ' User connected: ' + clientData.nickname + ' - Welcome!');
 	//And give something to the debugging-person that he/she can read out on his/her CLI
-	console.log('User ' + clientName + ' connected.');
+	console.log('User ' + clientData.nickname + ' connected.');
 	});
 
 	//When people get tired of the chat, we should clean up the mess we made with all the variables
@@ -173,6 +299,23 @@ io.on('connection', function(socket){
 });	//end io.on()
 
 //And at the very end (which is.. a weird but working structure) we start the actual HTTP Server.
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+//And connect our sexy MySQL Database (to a dark corner of the universe with Cloudant :))
+http.listen(port, function(){
+  console.log('listening on *:'+port);
+
+
+
+//What the pool does is basically store the database credentials and create connections on demand.
+//If it detects an active connection, no new ones are created.
+	pool = mysql.createPool({
+	host : 'us-cdbr-iron-east-03.cleardb.net',
+	user : 'bd7ae55beb6637',
+	password : 'c8e6983b',
+	database : 'ad_2a0407ab9b3bc7d'	
+});
+  if(pool){
+	  console.log("Database Parameters set.");
+  }
+
+
 });
--- public/javascripts/enternickname.js
+++ public/javascripts/enternickname.js
@@ -4,22 +4,84 @@
 
 //Simple script that saves a text from a form into the HTML5 Session Storage and redirects to the chatpage.
 //Refuses to redirect if nickname is invalid.
-$(document).ready(function() {
 
-	$("#sendNick").click(function(){
+function registerUser(){
+	
+	var newnickname = $("input").get(0).value;
+	var newpassword = $("input").get(1).value;
+	
+	
+	
+	socket = io();
+	socket.emit('register',{username : newnickname, userpassword : newpassword});
+	socket.on('registrationresult',function(data){
+		
+		if(data.success === "true"){
+			checkNick();
+		}
+		
+		else{
+			alert("Error during registration - verify inputs!");
+		}
+		
+		
+	});
+	
+}
+
+function checkNick(){
+	//$("#sendNick").click(function(){
+	//check for whitespaces..
 	  var checkName = $("input").get(0).value;
+	  if(checkName.indexOf(' ') >= 0){
+		  alert('Please refrain from using whitespaces in your nickname.');
+		  checkName = null;
+		  $(location).attr('href', '/');
+	  }
+	  //Grab password and create a socket
+	  var checkPassword = $("input").get(1).value;
+	  var socket = io();
+	  //Commence validation
+	  socket.emit('validateuserdata',{username : checkName, userpassword : checkPassword});
+	  //Handle result
+	  socket.on('validationresult',function(data){
+		  
+		  //Should be pretty clear what is happening. Yes, appending the name into the sessionStorage
+		  //is redundant at this point, but I reeaaaaalllyyyyyy don't want to change the entire program again..
+		  //"It's not stupid if it works." - The Internet
+		  if(data.success === "true"){
+			  console.log("Successfully authenticated");
+			  sessionStorage.chatNickName = checkName;
+			  $(location).attr('href', '/chat.html');
+			  
+		  }
+		  else{
+			  alert("Invalid username or password!");
+			  checkName = null;
+			  checkPassword = null;
+			  $(location).attr('href', '/');
+		  }
+		  
+	  });
 	  
-	  if(checkName != null){  
+	  
+	  
+	/*  if(checkName != null){  
 		if(checkName.indexOf(' ') < 0){
 		sessionStorage.chatNickName = checkName;
+		sessionStorage.chatPassword = checkPassword;
 		console.log(sessionStorage.chatNickName);
 		$(location).attr('href', '/chat.html');
 		} 
 		else {
 		alert('Please refrain from using whitespaces in your nickname.');
 		}
-	  }
+	  }*/
 	  
 	  
-    });
+    }//);
+
+
+$(document).ready(function() {
+	//$("sendNick".bind("click",checkData));
 });
